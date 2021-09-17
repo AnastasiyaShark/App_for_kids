12be695c9703e4238fc425809b554a41a4ce5888
Anastasiya Sharkouskaya <a.sharkovskaya@yandex.ru>
2021-09-17 19:32:54 +0200

    nåt ny

---
+++ App_for_kids/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_for_kids
+{
+    public partial class welcome_screen : Form
+    {
+        public welcome_screen()
+        {
+            InitializeComponent();
+        }
+    }
+}
